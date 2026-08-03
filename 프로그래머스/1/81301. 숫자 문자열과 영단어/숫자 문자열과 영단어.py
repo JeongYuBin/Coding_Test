a87fb1c48aa0a1cc48822c9e01d6fa45ded48c39
YuBin Jeong <73346564+JeongYuBin@users.noreply.github.com>
2026-08-03 16:05:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,51 +1,50 @@
-# 시작하는 단어
-# z : zero
-# o : one
-# t : two, three
-# f : four, five
-# s : six, seven
-# e : eight
-# n : nine
+# def solution(s):
+#     answer = ''
+#     check = 0
+#     while check < len(s):
+#         if s[check] >= '0' and s[check] <= '9':
+#             answer += s[check]
+#             check += 1
+#         elif s[check] == 'z':
+#             answer += '0'
+#             check += 4
+#         elif s[check] == 'o':
+#             answer += '1'
+#             check += 3
+#         elif s[check] == 'e':
+#             answer +='8'
+#             check += 5
+#         elif s[check] =='n':
+#             answer += '9'
+#             check += 4
+#         elif s[check] == 't':
+#             if s[check+1] == 'w':
+#                 answer += '2'
+#                 check += 3
+#             else:
+#                 answer += '3'
+#                 check += 5
+#         elif s[check] == 'f':
+#             if s[check+1] == 'o':
+#                 answer += '4'
+#                 check += 4
+#             else:
+#                 answer += '5'
+#                 check += 4
+#         elif s[check] == 's':
+#             if s[check+1] == 'i':
+#                 answer += '6'
+#                 check += 3
+#             else:
+#                 answer += '7'
+#                 check += 5
+        
+#     return int(answer)
 
 def solution(s):
-    answer = ''
-    check = 0
-    while check < len(s):
-        if s[check] >= '0' and s[check] <= '9':
-            answer += s[check]
-            check += 1
-        elif s[check] == 'z':
-            answer += '0'
-            check += 4
-        elif s[check] == 'o':
-            answer += '1'
-            check += 3
-        elif s[check] == 'e':
-            answer +='8'
-            check += 5
-        elif s[check] =='n':
-            answer += '9'
-            check += 4
-        elif s[check] == 't':
-            if s[check+1] == 'w':
-                answer += '2'
-                check += 3
-            else:
-                answer += '3'
-                check += 5
-        elif s[check] == 'f':
-            if s[check+1] == 'o':
-                answer += '4'
-                check += 4
-            else:
-                answer += '5'
-                check += 4
-        elif s[check] == 's':
-            if s[check+1] == 'i':
-                answer += '6'
-                check += 3
-            else:
-                answer += '7'
-                check += 5
-        
-    return int(answer)
+    
+    num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    for key, value in num_dic.items():
+        s = s.replace(key, value)
+
+    return int(s)
