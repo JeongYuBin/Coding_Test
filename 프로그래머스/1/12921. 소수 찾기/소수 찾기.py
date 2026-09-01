b9417667a42bdc50d6f2798fdf920e1d73fc9897
YuBin Jeong <73346564+JeongYuBin@users.noreply.github.com>
2026-09-01 14:19:27 +0900

    [level 1] Title: 소수 찾기, Time: 127.59 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,12 +1,12 @@
 def solution(n):
-    answer = 0
-    for i in range(2,n+1):
-        value = 0 
-        for k in range(2, int(i**0.5)+1):
-            if i % k == 0:
-                value = 1
-                break
-        if value == 0:
-            answer += 1
-            
-    return answer
+    prime = [True] * (n + 1)
+
+    prime[0] = False
+    prime[1] = False
+
+    for i in range(2, n + 1):
+        if prime[i]:
+            for k in range(i * 2, n + 1, i):
+                prime[k] = False
+
+    return prime.count(True)
