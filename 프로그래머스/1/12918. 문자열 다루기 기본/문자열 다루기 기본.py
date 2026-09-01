8bb66e9297f01b784c0dd79cdbc8f9e09d205e4f
YuBin Jeong <73346564+JeongYuBin@users.noreply.github.com>
2026-09-01 15:37:37 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,9 +1,17 @@
+# def solution(s):
+#     if len(s) != 4 and len(s) != 6:
+#         return False
+    
+#     try: 
+#         int(s)
+#         return True
+#     except ValueError:
+#         return False
+
 def solution(s):
     if len(s) != 4 and len(s) != 6:
-        return False
-    
-    try: 
-        int(s)
+        return False 
+    if s.isdigit():
         return True
-    except ValueError:
+    else:
         return False
